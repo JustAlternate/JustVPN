752b82c1e94178e5fa655f796a156a2ab2ee1f16
JustAlternate <lololollololol890@gmail.com>
2025-01-16 19:11:59 +0100

    feat: iterate on the basic api

--- src/main.go
+++ src/main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+func TerraformRessource(){
+	cmd := exec.Command("terraform apply", fmt.Sprintf("-var=\"endpoint=%s\" -var-file=\"secrets.tfvars\" --auto-approve"))
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getStart(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Creating the ressource...\n")
+	ip := r.Form.Get("IP")
+	parsedIp := net.ParseIP(ip)
+	if parsedIp == nil {
+		io.WriteString(w, "Not a valid ip address.\n")
+		return
+	}
+	timeWantedBeforeDeletion := r.Form.Get("timeWantedBeforeDeletion")
+	parsedTimeBeforeDeletion, err := strconv.Atoi(timeWantedBeforeDeletion)
+	if err != nil {
+		io.WriteString(w, "Not a valid time wanted address.\n")
+		return
+	}
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
-
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/start", getStart)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
