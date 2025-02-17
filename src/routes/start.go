d3ccc049a3b3abf0f12e0cc09e760e9a073cc8b6
JustAlternate <loicw@justalternate.fr>
2025-02-17 20:53:13 +0000

    Revert "feat: log streaming"
    This reverts commit 75b0a2ee1f374b09e53f0396cb9fabae529312ec.

--- src/routes/start.go
+++ src/routes/start.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -13,72 +12,25 @@ import (
 	"JustVPN/src/terraform"
 )
 
-type LogWriter struct {
-	w http.ResponseWriter
-	flusher http.Flusher
-}
-
-func (lw *LogWriter) Write(p []byte) (n int, err error) {
-	data := struct {
-		Type string `json:"type"`
-		Message string `json:"message"`
-	}{
-		Type: "log",
-		Message: string(p),
-	}
-	
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return 0, err
-	}
-	
-	fmt.Fprintf(lw.w, "data: %s\n\n", jsonData)
-	lw.flusher.Flush()
-	return len(p), nil
-}
-
 func GetStart(w http.ResponseWriter, r *http.Request) {
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		http.Error(w, "Streaming not supported", http.StatusInternalServerError)
-		return
-	}
-
-	// Set headers for SSE
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	// Create custom log writer
-	logWriter := &LogWriter{w: w, flusher: flusher}
-	log.SetOutput(logWriter)
-
-	terraformService := terraform.NewTerraformService()
 	log.Println("Creating TerraformService and Init...")
+	terraformService := terraform.NewTerraformService()
 
 	log.Println("Parsing Response information...")
 	err := r.ParseForm()
 	if err != nil {
-		log.Printf("Error parsing the response: %v\n", err)
-		return
+		log.Fatalf("Error parsing the response: %v", err)
 	}
-
 	ip := r.Form.Get("IP")
 	parsedIp := net.ParseIP(ip)
 	if parsedIp == nil {
-		log.Println("Not a valid ip address.")
-		return
+		log.Fatalf("Not a valid ip address.")
 	}
 	parsedIpString := parsedIp.String()
-	
 	timeWantedBeforeDeletion := r.Form.Get("timeWantedBeforeDeletion")
 	timeBeforeDeletion, err := strconv.Atoi(timeWantedBeforeDeletion)
 	if err != nil {
-		log.Printf("Not a valid time wanted address: %v\n", err)
-		return
+		log.Fatalf("Not a valid time wanted address: %v", err)
 	}
 
 	region := r.Form.Get("region")
@@ -98,23 +50,20 @@ func GetStart(w http.ResponseWriter, r *http.Request) {
 		"au-mel",
 		"br-gru",
 	}
-	if !slices.Contains(availableRegion, region) {
-		log.Println("Not a valid region")
-		return
+	if !slices.Contains(availableRegion, region){
+		log.Fatalf("Not a valid region")
 	}
 
 	log.Printf("Terraform Apply for %s %s...\n", parsedIpString, region)
 	err = terraformService.Apply(parsedIpString, region)
 	if err != nil {
-		log.Printf("Error: %v\n", err)
-		return
+		log.Fatal(err)
 	}
 
 	log.Printf("Getting hostIp for %s %s...\n", parsedIpString, region)
 	hostIp, err := terraformService.GetOutput()
-	if err != nil {
-		log.Printf("Error when retrieving the host ip: %s\n", err)
-		return
+if err != nil {
+		log.Fatalf("Error when retrieving the host ip: %s", err)
 	}
 
 	log.Printf("Getting PubKey for %s %s...\n", parsedIpString, region)
@@ -122,42 +71,29 @@ func GetStart(w http.ResponseWriter, r *http.Request) {
 	maxRetries := 5
 
 	for attempt := 1; attempt <= maxRetries || maxRetries == 0; attempt++ {
-		if err == nil {
-			break
-		}
-		log.Printf("Attempt %d: Error occurred when fetching pubkey, retrying in 10 seconds...\n", attempt)
-		time.Sleep(10 * time.Second)
-		pubkey, err = terraformService.GetPubKey(hostIp)
+			if err == nil {
+					break
+			}
+			log.Printf("Attempt %d: Error occurred when fetching pubkey, retrying in 10 seconds...\n", attempt)
+			time.Sleep(10 * time.Second)
+			pubkey, err = terraformService.GetPubKey(hostIp)
 	}
 
 	if err != nil {
-		log.Printf("Error when retrieving the pub key: %s\n", err)
-		return
+		log.Fatalf("Error when retrieving the pub key: %s", err)
 	}
+
 
 	log.Printf("Creating the response for %s %s...\n", parsedIpString, region)
 	response := map[string]string{
-		"host_endpoint": hostIp,
-		"public_key":    pubkey,
+			"host_endpoint": hostIp,
+			"public_key":    pubkey,
 	}
-
-	// Send final response
-	finalResponse := struct {
-		Type string `json:"type"`
-		Data map[string]string `json:"data"`
-	}{
-		Type: "result",
-		Data: response,
-	}
-	
-	jsonData, err := json.Marshal(finalResponse)
-	if err != nil {
-		log.Printf("Error encoding final response: %v\n", err)
-		return
-	}
-	
-	fmt.Fprintf(w, "data: %s\n\n", jsonData)
-	flusher.Flush()
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	json.NewEncoder(w).Encode(response)
 
 	log.Printf("Launching timer before destroy for %s %s...\n", parsedIpString, region)
 	go terraformService.Destroy(parsedIpString, timeBeforeDeletion)
